fe926b2bbef27d5cc835d98e9ec9ec3d023be881
Charles Ren <cren04@syr.edu>
2024-01-04 21:46:30 -0800

    Updated readme

--- spec/features/search_fire_nation_memers_spec.rb
+++ spec/features/search_fire_nation_memers_spec.rb
@@ -1,3 +1,20 @@
+=begin
+  As a user,
+When I visit "/"
+And I Select "Fire Nation" from the select field
+(Note: Use the existing select field)
+And I click "Search For Members"
+Then I should be on page "/search"
+Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
+And I should see a list with the detailed information for the first 25 members of the Fire Nation.
+
+And for each of the members I should see:
+- The name of the member (and their photo, if they have one)
+- The list of allies or "None"
+- The list of enemies or "None"
+- Any affiliations that the member has
+=end
+
 require 'rails_helper'
 
 RSpec.describe 'Search for Fire Nation members', type: :feature do
@@ -7,10 +24,7 @@ RSpec.describe 'Search for Fire Nation members', type: :feature do
     select 'Fire Nation', from: 'nation' 
     click_button 'Search For Members'
   end
-  # And for each of the members I should see:
-  # - The name of the member (and their photo, if they have one)
-  # - The list of allies or "None"
-  # - The list of enemies or "None"
+
   it 'allows the user to search for Fire Nation members' do
     expect(current_path).to eq('/search')
     
